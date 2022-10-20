70dc0ea6a514bbc5853df9944a75371c6d837498
Hsin-tingCHI <chihsinting@gmail.com>
2022-10-20 21:11:22 +0200

    Format js file

--- 01-JavaScript-Drum-Kit/app.js
+++ 01-JavaScript-Drum-Kit/app.js
@@ -1,20 +1,20 @@
-const keys = document.querySelectorAll('.key')
-const audios = document.querySelectorAll('audio')
+const keys = document.querySelectorAll('.key');
+const audios = document.querySelectorAll('audio');
 
 window.addEventListener('keydown', (e) => {
-    const downKey = e.keyCode
+    const downKey = e.keyCode;
     keys.forEach(key => {
         if (downKey == key.getAttribute('data-key')) {
-            key.classList.toggle('playing')
+            key.classList.toggle('playing');
             setTimeout(() => {
-                key.classList.toggle('playing')
-            }, 300)
+                key.classList.toggle('playing');
+            }, 300);
         }
-    })
+    });
     audios.forEach(audio => {
         if (downKey == audio.getAttribute('data-key')) {
-            audio.currentTime = 0
-            audio.play()
+            audio.currentTime = 0;
+            audio.play();
         }
-    })
-})
+    });
+});
--- 03-CSS-Variables/app.js
+++ 03-CSS-Variables/app.js
@@ -7,15 +7,15 @@ const blendMode = document.getElementById("blend-mode");
 
 spacing.addEventListener('change', () => {
     root.style.setProperty('--spacing', `${spacing.value}px`);
-})
+});
 
 blur.addEventListener('change', () => {
     root.style.setProperty('--blur', `${blur.value}px`);
-})
+});
 
 base.addEventListener('change', () => {
     root.style.setProperty('--base', base.value);
-})
+});
 
 blendMode.addEventListener('change', () => {
     root.style.setProperty('--blend-mode', blendMode.value);
